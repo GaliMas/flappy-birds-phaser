2ad565f81b33a6fb52a837bfcc94aa416d6dd9fe
GaliMas <jorgedgm11@gmail.com>
2017-02-21 21:11:47 +0100

    refact and out of bounds

--- js/juego.js
+++ js/juego.js
@@ -1,86 +1,84 @@
-PlayState = {};
+PlayState = {
+	game:null,
+	background: null,
+	player: null,
+	keys: null,
+	pipesUp: null,
+	pipesDown: null,
+	labelScore: null,
 
-PlayState.preload = function () {
-	this.game.load.image('player', 'assets/ball.png');
-	this.game.load.image('pipeUp', 'assets/pipeup.png');
-	this.game.load.image('pipeDown', 'assets/pipedown.png');
-	this.game.load.image('background', 'assets/sky.png');
-}
+	preload : function () {
+		this.game.load.image('player', 'assets/ball.png');
+		this.game.load.image('pipeUp', 'assets/pipeup.png');
+		this.game.load.image('pipeDown', 'assets/pipedown.png');
+		this.game.load.image('background', 'assets/sky.png');
+	},
 
-PlayState.create = function () {
-	
-	var restartGame = function(){
+	create : function () {
+	 	this.background = this.game.add.sprite(0, 0,'background');
+	    this.player = this.game.add.sprite(50, 225,'player');
+
+	    this.background.height = this.game.height;
+	    this.background.width = this.game.width;
+
+	    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+	    this.game.physics.arcade.enable(this.player);
+	    this.player.body.gravity.y = 2000;
+		
+	    this.game.physics.setBoundsToWorld();
+        this.player.checkWorldBounds = true;
+        this.player.events.onOutOfBounds.add(this.restartGame.bind(this), this);
+
+	    this.keys = this.game.input.keyboard.addKeys({
+	        space: Phaser.KeyCode.SPACEBAR
+	    });
+
+	    this.keys.space.onDown.add(function () {
+	        this.player.body.velocity.y = -600;
+	    }, this);
+
+		this.pipesUp = this.game.add.group(); 
+		this.pipesDown = this.game.add.group(); 
+
+		this.initScore();
+
+	    this.timer = this.game.time.events.loop(1000, this.addPipes, this); 
+	    this.addPipes();
+	},
+
+	update : function () {
+		this.game.physics.arcade.overlap(this.pipesUp, this.player, this.restartGame.bind(this));
+	    this.game.physics.arcade.overlap(this.pipesDown, this.player, this.restartGame.bind(this));
+	},
+
+	initScore(){
+		this.score = 0;
+	    this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });   
+	},
+
+	restartGame : function(){
 		this.game.state.start('main');
+	},
+
+	addPipes : function() {
+		this.addPipe(500, -50, 'pipeUp', this.pipesUp);
+		this.addPipe(500, 300, 'pipeDown', this.pipesDown);
+		this.labelScore.text = this.score++;
+	},
+
+	addPipe : function(x, y, pipeType, pipeGroup) {
+	    var pipe = this.game.add.sprite(x, y, pipeType);
+	    this.game.physics.arcade.enable(pipe);
+
+		pipe.height = 200;
+	    pipe.body.velocity.x = -600; 
+	    pipe.checkWorldBounds = true;
+	    pipe.outOfBoundsKill = true;
+
+	    pipeGroup.add(pipe);
 	}
-
-
-    this.background = this.game.add.sprite(0, 0,'background');
-    this.player = this.game.add.sprite(50, 225,'player');
-    this.game.physics.setBoundsToWorld();
-    this.player.events.onOutOfBounds.add(restartGame, this);
-
-	
-
-    this.background.height = this.game.height;
-    this.background.width = this.game.width;
-
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
-    this.game.physics.arcade.enable(this.player);
-    this.player.body.gravity.y = 2000;
-
-    this.keys = this.game.input.keyboard.addKeys({
-        space: Phaser.KeyCode.SPACEBAR
-    });
-
-    this.keys.space.onDown.add(function () {
-        this.player.body.velocity.y = -600;
-    }, this);
-
-	this.pipesUp = this.game.add.group(); 
-	this.pipesDown = this.game.add.group(); 
-
-	this.score = 0;
-    this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });   
-
-	var addPipe =  (function(x, y) {
-	    var pipeUp = this.game.add.sprite(500, -50, 'pipeUp');
-	    var pipeDown = this.game.add.sprite(500, 300, 'pipeDown');
-		pipeUp.height = 200;
-	    pipeDown.height = 200;
-	    
-	    this.pipesUp.add(pipeUp);
-	    this.pipesDown.add(pipeDown);
-
-	    this.game.physics.arcade.enable(pipeUp);
-	    this.game.physics.arcade.enable(pipeDown);
-
-	    
-	    pipeUp.body.velocity.x = -600; 
-	    pipeDown.body.velocity.x = -600; 
-
-	    
-	    pipeUp.checkWorldBounds = true;
-	    pipeUp.outOfBoundsKill = true;
-	    pipeDown.checkWorldBounds = true;
-	    pipeDown.outOfBoundsKill = true;
-
-	    this.labelScore.text = this.score++;
-	}).bind(this);
-
-    this.timer = this.game.time.events.loop(1000, addPipe, this); 
-    addPipe();
-    
-};
-
-PlayState.update = function () {
-	var restartGame = function(){
-		this.game.state.start('main');
-	}
-
-	this.game.physics.arcade.overlap(this.pipesUp, this.player, restartGame.bind(this));
-    this.game.physics.arcade.overlap(this.pipesDown, this.player, restartGame.bind(this));
 };
 
 window.onload = function () {
